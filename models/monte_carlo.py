c7530002da9c6237741b89ce73e19299f75ba9e6
amrit1610-fin <amritanshuofficial718@gmail.com>
2026-09-14 21:34:04 +0530

    GBM to Student t-shocks in monte carlo

--- models/monte_carlo.py
+++ models/monte_carlo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import logging
+from scipy.stats import t
 from .base import BaseVaRModel
 
 logger = logging.getLogger(__name__)
@@ -10,9 +11,11 @@ class MonteCarloVaR(BaseVaRModel):
     Calculates VaR and ES using Monte Carlo Simulation with Geometric Brownian Motion (GBM)
     and Cholesky decomposition for asset correlation.
     """
-    def __init__(self, confidence_level: float = 0.99, portfolio_value: float = 1.0, num_simulations: int = 10000):
+    def __init__(self, confidence_level: float = 0.99, portfolio_value: float = 1.0,
+                 num_simulations: int = 10000, dof: int = 5):
         super().__init__(confidence_level, portfolio_value)
         self.num_simulations = num_simulations
+        self.dof = dof
         np.random.seed(42) 
 
     def calculate(self, returns: pd.DataFrame, weights: np.ndarray) -> dict:
@@ -34,8 +37,11 @@ class MonteCarloVaR(BaseVaRModel):
             cov_matrix = LedoitWolf().fit(returns).covariance_
             L = np.linalg.cholesky(cov_matrix)
             
-        Z = np.random.standard_normal((self.num_simulations, num_assets))   # Generating Independent Random Shocks
-        correlated_shocks = Z @ L.T                                         # Correlating the Shocks
+        # Generating the underlying process using Student's t-statistics
+        raw_t_shocks = t.rvs(df=self.dof, size=(self.num_simulations, num_assets))
+        scaling_factor = np.sqrt((self.dof - 2) / self.dof)
+        standardized_t_shocks = raw_t_shocks * scaling_factor               # Standardize Variance
+        correlated_shocks = standardized_t_shocks @ L.T                                         # Correlating the Shocks
         simulated_asset_returns = drift + correlated_shocks                 # Simulating 1-Day Log Returns
         simulated_port_returns = simulated_asset_returns @ weights          # Simulated Portfolio Returns
         
